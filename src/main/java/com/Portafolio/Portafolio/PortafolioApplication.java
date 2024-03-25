315989b756eae697958a97355864bc1023464e40
Rafael Brieva <rafael2019bp@gmail.com>
2024-03-25 20:45:10 +0100

    Enrutamiento

--- src/main/java/com/Portafolio/Portafolio/PortafolioApplication.java
+++ src/main/java/com/Portafolio/Portafolio/PortafolioApplication.java
@@ -2,12 +2,32 @@ package com.Portafolio.Portafolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PortafolioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortafolioApplication.class, args);
-	}
-
+    // Este método estará accesible en la ruta "/"
+    @RequestMapping(value = "/inicio")
+    public String index() {
+        return "<h1>Bienvenidos !</h1>";
+    }
+    
+    // Este método estará accesible en la ruta "/menu"
+    @RequestMapping(value = "/menu")
+    public String menu() {
+        return "<h1>Menu Principal</h1>";
+    }
+    
+    // Este método estará accesible en la ruta "/fin"
+    @RequestMapping(value = "/fin")
+    public String fin() {
+        return "<h1>Fin Aplicación</h1>";
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PortafolioApplication.class, args);
+    }
 }
